1c7fe3bd1730d35517d2f6280bbd73d5b0b59edf
AR17 <89924873+Kepryx@users.noreply.github.com>
2023-05-29 01:49:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let weather = {
-    "apikey": WEATHER_API_KEY,
+    "apikey": 11d9e43e458583659839ce045458f316,
     fetchWeather : function (city) {
         fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
         + city 
@@ -39,4 +39,4 @@ document.querySelector(".searchbar").addEventListener("keyup", function(event){
     }
 });
 
-weather.fetchWeather("durgapur");
+weather.fetchWeather("puducherry");
